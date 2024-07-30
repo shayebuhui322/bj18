35b0cba56d5c52df16865471711fcd9c73383daf
shayebuhui322 <1634349121@qq.com>
2024-07-30 10:01:31 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-
+from django.shortcuts import redirect
 
 def index(request):
     return HttpRespones('index')
+def login(request):
+    return redirect('/index')
